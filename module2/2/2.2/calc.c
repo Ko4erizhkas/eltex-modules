1833a87b374978f9bddfb8aa3b802911704bc348
Ko4erizhkas <fit12347@gmail.com>
2026-07-24 14:22:19 +0700

    #2
    Задание 2.2 сделано

--- module2/2/2.2/calc.c
+++ module2/2/2.2/calc.c
@@ -1,12 +1,13 @@
 #include "calc.h"
 #include <stdio.h>
-static double sum (double n1, double n2){ return n1 + n2; }
-static double sub (double n1, double n2){ return n1 - n2; }
+static double sum (double n1, double n2) { return n1 + n2; }
+static double sub (double n1, double n2) { return n1 - n2; }
 static double mult (double n1, double n2) {return n1 * n2; }
 static double divide (double n1, double n2)
 {
     return n1 / n2;
 }
+
 void interface_calc(void)
 {
     for (;;)
